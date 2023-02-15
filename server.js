5234868ea4aca0e8f2c5d8740c38c4a472467398
jawkneewin <johnnytnguyen93@gmail.com>
2023-02-15 09:39:14 -0600

    removed all examples in files

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const artworkRoutes = require('./app/routes/artwork_routes')
 const exhibitionRoutes = require('./app/routes/exhibition_routes')
@@ -67,7 +66,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(artworkRoutes)
 app.use(exhibitionRoutes)
