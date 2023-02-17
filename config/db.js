c3480ddd48c75700824566548159ab011a66a676
caitreid <ctlnrd@gmail.com>
2023-02-17 09:13:43 -0500

    dept show route

--- config/db.js
+++ config/db.js
@@ -18,4 +18,6 @@ const localDb = process.env.TESTENV ? database.test : database.development
 // heroku production evironment otherwise use test or development db
 const currentDb = process.env.MONGODB_URI || localDb
 
+console.log(currentDb)
+
 module.exports = currentDb
